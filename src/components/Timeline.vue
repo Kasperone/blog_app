b53c424ccc7199164442ade5f6143ea7d0960b19
kasperone <kasper.kosecki@antologic.com>
2023-05-13 23:11:41 +0200

    feat(BA-1): created time-line component

---
+++ src/components/Timeline.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const periods = ['Today', 'This Week', 'This Month']
+
+const selectedPeriod = ref('Today')
+
+const selectPeriod = (period: string) => {
+  console.log(period)
+  selectedPeriod.value = period
+}
+</script>
+
+<template>
+  <nav  class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+
+      <a :class="{ 'is-active': period === selectedPeriod }"
+         v-for="(period, index) of periods"
+         :key="`${index} '+' ${period}`"
+         @click="selectPeriod(period)">
+        {{ period }}
+      </a>
+
+    </span>
+  </nav>
+</template>
+
+<style scoped>
+</style>
