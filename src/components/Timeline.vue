11793012813e3170afdfbf19c87df550cb0e4fff
kasperone <kasper.kosecki@antologic.com>
2023-05-15 22:58:10 +0200

    feat(BA-2): created time-line-item component

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,29 +1,56 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref ,computed } from 'vue'
+import { TimelinePost, today, thisWeek, thisMonth } from "../posts";
+import { DateTime } from "luxon";
+import TimelineItem from "./TimelineItem.vue";
 
-const periods = ['Today', 'This Week', 'This Month']
+const periods = ['Today', 'This Week', 'This Month'] as const
+type Period = typeof periods[number]
+const selectedPeriod = ref<Period>('Today')
 
-const selectedPeriod = ref('Today')
-
-const selectPeriod = (period: string) => {
-  console.log(period)
+const selectPeriod = (period: Period) => {
   selectedPeriod.value = period
 }
+
+const posts = computed<TimelinePost[]>(() => {
+  return [
+    today,
+    thisWeek,
+    thisMonth
+  ].map(post => {
+    return {...post, created: DateTime.fromISO(post.created)}
+  }).filter(post => {
+    if (selectedPeriod.value === 'Today') {
+      return post.created >= DateTime.now().minus({day: 1})
+    }
+
+    if (selectedPeriod.value === 'This Week') {
+      return post.created >= DateTime.now().minus({week: 1})
+    }
+
+    return post
+  })
+})
+
 </script>
 
 <template>
   <nav  class="is-primary panel">
     {{ selectedPeriod }}
     <span class="panel-tabs">
-
       <a :class="{ 'is-active': period === selectedPeriod }"
          v-for="(period, index) of periods"
          :key="`${index} '+' ${period}`"
          @click="selectPeriod(period)">
         {{ period }}
       </a>
-
     </span>
+
+    <TimelineItem
+       v-for="post in posts"
+       :key="post.id"
+       :post="post"
+    />
   </nav>
 </template>
 
