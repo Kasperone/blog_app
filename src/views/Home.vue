42d949ef47d2e241b38155739fff26239c84e5e7
kasperone <kasper.kosecki@antologic.com>
2024-07-09 23:31:17 +0200

    feat(BA-6): added router-view

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Timeline from "../components/Timeline.vue";
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Timeline />
+    </template>
+    <template #fallback>Loading...</template>
+  </Suspense>
+</template>
